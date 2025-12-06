7ac8c0a1d7efdc8262a32eebe02923e3dcde2231
Lakshmi Priya <lakshpri@gmail.com>
2025-12-06 19:49:10 +0530

    your message

--- DSA/Striver_A2Z/Step1_Basics/Fibonacci_Number.java
+++ DSA/Striver_A2Z/Step1_Basics/Fibonacci_Number.java
@@ -1,3 +1,27 @@
 public class Fibonacci_Number {
-    
+
+    public static void main(String[] args) {
+
+        int n = 10; // change to get nth Fibonacci
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        int a = 0;
+        int b = 1;
+        int fib = 0;
+
+        for (int i = 2; i <= n; i++) {
+            fib = a + b;
+            a = b;
+            b = fib;
+        }
+
+        System.out.println("Fibonacci number at position " + n + " = " + fib);
+    }
 }
