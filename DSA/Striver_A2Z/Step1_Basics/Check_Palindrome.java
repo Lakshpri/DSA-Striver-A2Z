ba72b67667ccc2481cefd56583de469cd5cc2b7f
Lakshmi Priya <lakshpri@gmail.com>
2025-12-06 19:47:49 +0530

    your message

--- DSA/Striver_A2Z/Step1_Basics/Check_Palindrome.java
+++ DSA/Striver_A2Z/Step1_Basics/Check_Palindrome.java
@@ -1,3 +1,21 @@
 public class Check_Palindrome {
-    
+
+    public static void main(String[] args) {
+
+        int n = 121; // change to test other numbers
+        int original = n;
+        int reversed = 0;
+
+        while (n > 0) {
+            int lastDigit = n % 10;
+            reversed = reversed * 10 + lastDigit;
+            n /= 10;
+        }
+
+        if (reversed == original) {
+            System.out.println(original + " is a Palindrome Number");
+        } else {
+            System.out.println(original + " is NOT a Palindrome Number");
+        }
+    }
 }
