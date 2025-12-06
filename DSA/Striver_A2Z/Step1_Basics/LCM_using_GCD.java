cd5057eaf12ef8bfefc96756911ddaeadcf6dadd
Lakshmi Priya <lakshpri@gmail.com>
2025-12-06 19:51:41 +0530

    your message

--- DSA/Striver_A2Z/Step1_Basics/LCM_using_GCD.java
+++ DSA/Striver_A2Z/Step1_Basics/LCM_using_GCD.java
@@ -1,3 +1,23 @@
 public class LCM_using_GCD {
-    
+
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 18;
+
+        int gcd = findGCD(a, b);
+        int lcm = (a * b) / gcd;
+
+        System.out.println("LCM of " + a + " and " + b + " = " + lcm);
+    }
+
+    // Euclid's Algorithm for GCD
+    static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 }
