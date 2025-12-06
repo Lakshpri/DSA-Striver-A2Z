c70764b38fcebf3fdf9f0a0dbdbf5e2d00de43b8
Lakshmi Priya <lakshpri@gmail.com>
2025-12-06 19:48:32 +0530

    your message

--- DSA/Striver_A2Z/Step1_Basics/Count_Digits.java
+++ DSA/Striver_A2Z/Step1_Basics/Count_Digits.java
@@ -1,9 +1,16 @@
 public class Count_Digits {
 
+    public static void main(String[] args) {
+
+        int n = 12345; // change to test other numbers
+        int count = 0;
+        int temp = n;
+
+        while (temp > 0) {
+            count++;
+            temp /= 10;
+        }
+
+        System.out.println("Number of digits in " + n + " = " + count);
+    }
 }
-
-
-
-
-
-
