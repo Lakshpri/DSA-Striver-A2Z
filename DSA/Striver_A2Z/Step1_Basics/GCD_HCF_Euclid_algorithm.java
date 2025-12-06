6fb235dafb52620d546a2e11f6dd74b6f3d386cc
Lakshmi Priya <lakshpri@gmail.com>
2025-12-06 19:50:56 +0530

    your message

--- DSA/Striver_A2Z/Step1_Basics/GCD_HCF_Euclid_algorithm.java
+++ DSA/Striver_A2Z/Step1_Basics/GCD_HCF_Euclid_algorithm.java
@@ -1,3 +1,19 @@
 public class GCD_HCF_Euclid_algorithm {
-    
+
+    public static void main(String[] args) {
+
+        int a = 48;
+        int b = 18;
+
+        int originalA = a;
+        int originalB = b;
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println("GCD of " + originalA + " and " + originalB + " = " + a);
+    }
 }
