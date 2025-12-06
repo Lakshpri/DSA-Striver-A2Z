37f8fc24ffb9a2bca2531eb4056d258889f0123b
Lakshmi Priya <lakshpri@gmail.com>
2025-12-06 19:46:15 +0530

    your message

--- DSA/Striver_A2Z/Step1_Basics/Armstrong_Numbers.java
+++ DSA/Striver_A2Z/Step1_Basics/Armstrong_Numbers.java
@@ -4,8 +4,7 @@ public class Armstrong_Numbers {
 
         int n = 153; // you can change the number
         int original = n;
-        
-        // Step 1: Count digits
+
         int digits = 0;
         int temp = n;
         while (temp > 0) {
@@ -13,13 +12,13 @@ public class Armstrong_Numbers {
             temp /= 10;
         }
 
-        // Step 2: Calculate sum of digits raised to power 'digits'
+        
         int sum = 0;
         temp = n;
         while (temp > 0) {
             int lastDigit = temp % 10;
 
-            // raise digit to power 'digits'
+        
             int power = 1;
             for (int i = 0; i < digits; i++) {
                 power *= lastDigit;
@@ -29,7 +28,7 @@ public class Armstrong_Numbers {
             temp /= 10;
         }
 
-        // Step 3: Check Armstrong condition
+        
         if (sum == original) {
             System.out.println(original + " is an Armstrong Number");
         } else {
--- DSA/Striver_A2Z/Step1_Basics/Check_for_Prime.java
+++ DSA/Striver_A2Z/Step1_Basics/Check_for_Prime.java
@@ -1,3 +1,25 @@
 public class Check_for_Prime {
-    
+
+    public static void main(String[] args) {
+
+        int n = 29; // change the number to test
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(n + " is a Prime Number");
+        } else {
+            System.out.println(n + " is NOT a Prime Number");
+        }
+    }
 }
